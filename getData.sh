e8383f2ef3fa71b050df0aaed770fe7019639db5
Evan D Young <evan.d.young@gmail.com>
2014-12-09 14:51:52 -0700

    adding shell script

---
+++ getData.sh
@@ -0,0 +1,7 @@
+for n in `seq 93 113`;
+do
+  mkdir ../$n
+  wget -np --no-clobber --reject='index.html*' -l1 --no-check-certificate --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://www.govtrack.us/data/us/$n/stats/
+  mv www.govtrack.us/data/us/$n/stats/ ../$n
+  rm ./*
+done
