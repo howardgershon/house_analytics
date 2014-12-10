c41122759837892a7c76d4e2a8a3ac0d8c517e08
Evan D Young <evan.d.young@gmail.com>
2014-12-10 16:04:59 -0700

    updating functions.r

--- functions.r
+++ functions.r
@@ -51,9 +51,28 @@ reg.gam = function(e){
 
 }
 
+get.people = function(num) {
+    people = xmlParse('http://kansascitystandard.com/house_analytics/house_analytics/data/people/people93.xml')
+    data = xmlToList(people)
+    district = c()
+    name = c()
+    for (i in 1:553) {
+        if (data[[i]]$.attr['title'][[1]] == 'Rep.'){
+            district[i] = paste0(data[[i]]$.attr['state'][[1]], '-', data[[i]]$.attr['district'][[1]])
+            name[i] = paste(data[[i]]$.attr['firstname'][[1]], data[[i]]$.attr['lastname'][[1]])
+        } else {
+            i = i + 1
+        }
+    }
+}
 
-get.latlon = function() {
-    API.key = AIzaSyBLPCwn4eQz6AYuSnlHqkUsxuDxvBWbPaE
-
+get.latlon = function(cd) {
+    API.key = 'AIzaSyBLPCwn4eQz6AYuSnlHqkUsxuDxvBWbPaE'
+    doc.id = '20174hYH1Ncitje-U2nxykmwFn6nilfpo_PjJRMcOk'
+    query = paste0('SELECT * FROM ', doc.id,' WHERE CD = ', cd)
+    url = paste0('https://www.googleapis.com/fusiontables/v1/query?', query, '&key=', API.key)
+    info = getURL(url)
+    lon = fromJSON(info)$rows[[1]]$geometry$coordinates[[1]][1]
+    lat = fromJSON(info)$rows[[1]]$geometry$coordinates[[1]][2]
 }
 
